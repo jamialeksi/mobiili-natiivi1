ecc2847cab46a6a718f13c3a8ccfd1fe168cfb1a
Jami Pietila <jami0809@windowslive.com>
2026-01-22 20:25:18 +0200

    viikko 2 tehtävä

--- app/src/main/java/com/example/viikko1/ui/HomeScreen.kt
+++ app/src/main/java/com/example/viikko1/ui/HomeScreen.kt
@@ -1,19 +1,24 @@
 package com.example.viikko1.ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.viikko1.domain.Task
-import com.example.viikko1.domain.mockTasks
-import com.example.viikko1.domain.filterByDone
-import com.example.viikko1.domain.sortByDueDate
+import com.example.viikko1.viewmodel.TaskViewModel
+//import java.time.LocalDate jotakin erroria antaa kun yritti puskea kun tätä käytti päivämääränä. EI SITTE
 
 @Composable
 fun HomeScreen() {
 
-    var currentList by remember { mutableStateOf(mockTasks) }
+    val vm: TaskViewModel = viewModel()
+    val currentList = vm.tasks
+
+    var title by remember { mutableStateOf("") }
 
     Column(modifier = Modifier.padding(16.dp)) {
 
@@ -22,64 +27,103 @@ fun HomeScreen() {
             style = MaterialTheme.typography.headlineMedium,
             modifier = Modifier.padding(bottom = 16.dp)
         )
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween) {
 
-            Button(onClick = {
-                currentList = mockTasks
-            }) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Button(onClick = { vm.showAll() }) {
                 Text("Kaikki")
             }
 
-            Button(onClick = {
-                currentList = filterByDone(mockTasks, true)
-            }) {
-                Text("Valmiit")
-            }
+            Button(onClick = { vm.filterByDone(true) }) { Text("Valmiit") }
 
-            Button(onClick = {
-                currentList = filterByDone(mockTasks, false)
-            }) {
-                Text("Kesken")
-            }
+            Button(onClick = { vm.filterByDone(false) }) { Text("Kesken") }
         }
 
         Spacer(modifier = Modifier.height(12.dp))
 
         Button(
             onClick = {
-                currentList = sortByDueDate(currentList)
+                vm.sortByDueDate()
             },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Järjestä päivämäärän mukaan")
         }
 
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row(Modifier.fillMaxWidth()) {
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                modifier = Modifier.weight(1f)
+            )
+            Button(onClick = {
+                val newId = (vm.tasks.maxOfOrNull { it.id } ?: 0) + 1
+                vm.addTask(
+                    Task(
+                        id = newId,
+                        title = title,
+                        description = "",
+                        priority = 1,
+                        dueDate = "2026-01-01", //täällä oli localdate käytössä
+                        done = false
+                    )
+                )
+                title = ""
+            }) {
+                Text("Lisää")
+            }
+        }
+
         Spacer(modifier = Modifier.height(20.dp))
 
-        // --- LISTA ---
-        currentList.forEach { task ->
-            TaskRow(task = task)
+        LazyColumn {
+            items(currentList) { task ->
+                TaskRow(
+                    task = task,
+                    onToggle = { vm.toggleDone(task.id) },
+                    onDelete = { vm.removeTask(task.id) }
+                )
+            }
         }
     }
 }
 
 @Composable
-fun TaskRow(task: Task) {
+fun TaskRow(
+    task: Task,
+    onToggle: () -> Unit,
+    onDelete: () -> Unit
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 4.dp)
     ) {
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Text(text = task.title, style = MaterialTheme.typography.titleMedium)
-            Text(text = task.description, style = MaterialTheme.typography.bodySmall)
-        }
-        Text(
-            text = if (task.done) "Valmis" else "Kesken",
-            modifier = Modifier.padding(start = 8.dp)
+        Checkbox(
+            checked = task.done,
+            onCheckedChange = { onToggle() }
         )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 6.dp)
+        ) {
+            Text(text = task.title)
+            Text(
+                text = task.dueDate,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+
+
+        Button(onClick = { onDelete() }) {
+            Text("Poista")
+        }
     }
 }
